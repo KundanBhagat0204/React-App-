5b04346900806425cb089bf46d7503046a5915bb
KundanBhagat0204 <kundanbhagat7777@gmail.com>
2025-02-20 18:53:57 +0530

    Adding Button.tsx

--- react_second/src/Components/Button.tsx
+++ react_second/src/Components/Button.tsx
@@ -1,3 +1,11 @@
+/**
+ * The Button component is a TypeScript React component that renders a button with specified color and
+ * onClick event handler.
+ * @param {Props}  - The `Button` component takes the following props:
+ * @returns The `Button` component is being returned. It is a functional component that renders a
+ * button element with the specified children, onClick event handler, and optional color prop. The
+ * color prop defaults to "primary" if not provided.
+ */
 interface Props {
   children: string;
   color?: "primary" | "secondary" | "dark";
