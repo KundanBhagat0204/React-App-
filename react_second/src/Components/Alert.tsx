c348fe3aa737ba1a29c7bb8b2730af06549a3f58
KundanBhagat0204 <kundanbhagat7777@gmail.com>
2025-02-20 18:43:48 +0530

    Added Alert.tsx for Alert Chap and Practics

--- react_second/src/Components/Alert.tsx
+++ react_second/src/Components/Alert.tsx
@@ -1,3 +1,14 @@
+/**
+ * The `Alert` component in TypeScript React displays an alert message with a close button that
+ * triggers the `onClose` function when clicked.
+ * @param {Props}  - The code snippet provided defines a React component called `Alert`. It takes two
+ * props:
+ * @returns The `Alert` component is being returned, which is a React functional component that
+ * displays an alert message with a close button. The component takes two props: `children` for the
+ * content of the alert message and `onClose` for the function to be called when the close button is
+ * clicked. The component renders the alert message along with a close button that triggers the
+ * `onClose` function when clicked
+ */
 import { ReactNode } from "react";
 
 interface Props {
