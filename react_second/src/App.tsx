5cb32a553240c80fdb76d6fb5f7db2a4cc7f983b
KundanBhagat0204 <kundanbhagat7777@gmail.com>
2025-02-21 12:19:02 +0530

    Adding List Group

--- react_second/src/App.tsx
+++ react_second/src/App.tsx
@@ -1,49 +1,49 @@
-// import ListGroup from "./Components/ListGroup";
+import ListGroup from "./Components/ListGroup";
 
-// function App() {
-//   let items = ["New York", "San francisco", "Tokyo", "London", "Paris"];
-//   //passing funtions via props
-//   const handleSelectItem = (item: string) => {
-//     console.log(item);
-//   };
-
-//   return (
-//     <div>
-//       <ListGroup
-//         items={items}
-//         heading="cities"
-//         onSelectedItem={handleSelectItem}
-//       />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-//Alert Passing Children
-import { useState } from "react";
-import Alert from "./Components/Alert";
-import Button from "./Components/Button";
-// function App() {
-//   return (
-//     <div className="alert alert-dark">
-//       <Alert>
-//         Hello <span>World</span>
-//       </Alert>
-//     </div>
-//   );
-// }
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+  let items = ["New York", "San francisco", "Tokyo", "London", "Paris"];
+  //passing funtions via props
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
-      )}
-      <Button color="primary" onClick={() => setAlertVisibility(true)}>
-        My Button
-      </Button>
+      <ListGroup
+        items={items}
+        heading="cities"
+        onSelectedItem={handleSelectItem}
+      />
     </div>
   );
 }
+
 export default App;
+
+// //Alert Passing Children
+// import { useState } from "react";
+// import Alert from "./Components/Alert";
+// import Button from "./Components/Button";
+// // function App() {
+// //   return (
+// //     <div className="alert alert-dark">
+// //       <Alert>
+// //         Hello <span>World</span>
+// //       </Alert>
+// //     </div>
+// //   );
+// // }
+// function App() {
+//   const [alertVisible, setAlertVisibility] = useState(false);
+//   return (
+//     <div>
+//       {alertVisible && (
+//         <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+//       )}
+//       <Button color="primary" onClick={() => setAlertVisibility(true)}>
+//         My Button
+//       </Button>
+//     </div>
+//   );
+// }
+// export default App;
