c5ab06972acf0227a0e10cf03e6cfbe518597719
KundanBhagat0204 <kundanbhagat7777@gmail.com>
2025-02-23 12:52:05 +0530

    Form Validation

--- react_second/src/Components/Form.tsx
+++ react_second/src/Components/Form.tsx
@@ -1,31 +1,44 @@
-import { FormEvent } from "react";
-import { useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 const Form = () => {
-  const [person, setPerson] = useState({
-    name: "",
-    age: 0,
-  });
+  interface FormData {
+    name: string;
+    age: number;
+  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(person);
-  };
+  //   const handleSubmit = (event: FormEvent) => {
+  //     event.preventDefault();
+  //     console.log(person);
+  //   };
+
+  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
           id="name"
-          onChange={(event) =>
-            setPerson({ ...person, name: event.target.value })
-          }
-          value={person.name}
+          //   onChange={(event) =>
+          //     setPerson({ ...person, name: event.target.value })
+          //   }
+          {...register("name", { required: true, minLength: 3 })}
+          // value={person.name}
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The Name field is required</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">The Name Must be three letter</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
@@ -33,13 +46,14 @@ const Form = () => {
         </label>
         <input
           type="number"
-          onChange={(event) =>
-            setPerson({
-              ...person,
-              age: parseInt(event.target.value),
-            })
-          }
-          value={person.age}
+          //   onChange={(event) =>
+          //     setPerson({
+          //       ...person,
+          //       age: parseInt(event.target.value),
+          //     })
+          //   }
+          {...register("age")}
+          //   value={person.age}
           id="age"
           className="form-control"
         ></input>
