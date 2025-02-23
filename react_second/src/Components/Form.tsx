2bdb754a52bd1d379670e1a324d19732187183d4
KundanBhagat0204 <kundanbhagat7777@gmail.com>
2025-02-23 14:19:17 +0530

    Disabling Button if the inputs are inValid

--- react_second/src/Components/Form.tsx
+++ react_second/src/Components/Form.tsx
@@ -14,7 +14,7 @@ const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FormData) => console.log(data);
@@ -45,7 +45,7 @@ const Form = () => {
         />
         {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
-      <button className="btn btn-primary" type="submit">
+      <button className="btn btn-primary" disabled={!isValid} type="submit">
         Submit
       </button>
     </form>
