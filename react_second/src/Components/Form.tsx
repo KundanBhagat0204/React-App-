4b701db3b59b2f045f0faac7e6ceed0ac49af1a5
KundanBhagat0204 <kundanbhagat7777@gmail.com>
2025-02-23 11:21:48 +0530

    Building Form

--- react_second/src/Components/Form.tsx
+++ react_second/src/Components/Form.tsx
@@ -7,6 +7,15 @@ const Form = () => {
         </label>
         <input id="name" type="text" className="form-control" />
       </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input type="number" id="age" className="form-control"></input>
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
     </form>
   );
 };
