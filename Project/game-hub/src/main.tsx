697434b564cf52d684d41d055f1d8bf4c4127c92
KundanBhagat0204 <kundanbhagat7777@gmail.com>
2025-02-25 16:42:23 +0530

    Removing Error

--- Project/game-hub/src/main.tsx
+++ Project/game-hub/src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Provider } from "@/components/ui/provider";
+import { Provider } from "@/components/ui/Provider";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
